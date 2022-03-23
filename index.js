583e70450f60df34270871cf98123324ba8f1945
Nícolas Gabriel <nickygamer023@gmail.com>
2022-03-23 16:48:22 -0300

    replacement system remade

--- index.js
+++ index.js
@@ -7,24 +7,22 @@ fs.readFile('README_BASE.md', 'utf-8', async (err, data) => {
 
     const numeroRandom = Math.round(Math.random() * 1000000)
 
+    const substituicoes = {
+        titulo: 'Dynamic Readme',
+        listaDeNomes: `- Carlos\n- Ana\n- Sérgio`,
+        numeroRandom: numeroRandom,
+    }
+
     const modificado = data
         .replace(
-            /%{titulo}/gm,
-            'Dynamic Readme'
-        )
-        .replace(
-            /%{listaDeNomes}/gm,
-            `- Carlos\n- Ana\n- Sérgio`
-        )
-        .replace(
-            /%{numeroRandom}/gm,
-            numeroRandom
+            /%{.*}/gm,
+            e => substituicoes?.[e.slice(2, -1)] || e
         )
 
     fs.writeFile('README.md', modificado, 'utf-8', (err) => {
         if (err) {
             throw err;
         }
-        console.log('Processo finalizado!');
+        console.log('✔ Processo finalizado!');
     });
 });
